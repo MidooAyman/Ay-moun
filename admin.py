43e0370eeca33a5dcf8fb69c9a8c3dca4ac9b679
MidooAyman <116003039+MidooAyman@users.noreply.github.com>
2023-04-25 09:53:09 +0200

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Destination
+from .models import Login
 
 # Register your models here.
-
-admin.site.register(Destination)
-
+admin.site.register(Login)
--- models.py
+++ models.py
@@ -1,31 +1,17 @@
 from django.db import models
 
 # Create your models here.
-
-class Destination(models.Model):
-   
-    name = models.CharField(max_length=100, verbose_name='Product Name')
-    img= models.ImageField(upload_to='pics')
-    ORIGINAL = 'OR'
-    MIRROR = 'MI'
-    EGYPTIAN = 'EG'
-    SHIRTS = 'SH'
-    JACKETS = 'JA'
-    Other = 'OT'
-    CHOICES = [
-        (ORIGINAL, 'Original'),
-        (MIRROR, 'Mirror'),
-        (EGYPTIAN, 'Egyptian'),
-        (SHIRTS, 'SHIRTS'),
-        (JACKETS, 'JACKETS'), 
-        (Other, 'Other'),
-    ]
-    des = models.CharField(max_length=2, choices=CHOICES)
-    price= models.IntegerField()
-    pricee = models.DecimalField(max_digits=6, decimal_places=2, default=0)
-    special= models.BooleanField(default=False)
-    later= models.BooleanField(default=False)
-    description = models.CharField(max_length=250)
+class Login(models.Model):
+    username = models.CharField(max_length=50, default='')
+    phone = models.CharField(max_length=50)
+    adress = models.CharField(max_length=50)
+    product_name = models.CharField(max_length=50)
+    size = models.CharField(max_length=50)
     
     def __str__(self):
-        return self.name
+        return self.username
+    
+    
+    
+    
+    
